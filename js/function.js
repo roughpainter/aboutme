ff89837910b3ca3d1e45e0b6124d0cba7ecfdc8f
jj.yamada <yamada@juliajapan.co.jp>
2020-03-19 17:39:20 +0900

    200318

--- js/function.js
+++ js/function.js
@@ -1,5 +1,9 @@
 /* function.js
 --------------------------------------------------------------------------------------------------------*/
+jQuery(function() {
+	objectFitImages('img.object-fit');
+});
+
 jQuery(window).load(function() {
 	$('#main-visual li').addClass('active');
 });
